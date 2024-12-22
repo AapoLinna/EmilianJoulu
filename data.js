d935d066fc34ae68425634112bc1fb378f1cb0a2
Aapo Linna <96585174+AapoLinna@users.noreply.github.com>
2024-12-22 22:07:04 +0200

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const countryData = {
   "Spain": 3.7,
 };
 
-// Simple GeoJSON for Finland and Spain
+// Simple GeoJSON for testing
 const europeGeoJSON = {
   "type": "FeatureCollection",
   "features": [
@@ -13,16 +13,16 @@ const europeGeoJSON = {
       "properties": { "name": "Finland" },
       "geometry": {
         "type": "Polygon",
-        "coordinates": [[[24.0, 60.0], [25.0, 60.0], [25.0, 61.0], [24.0, 61.0], [24.0, 60.0]]]
-      }
+        "coordinates": [[[24.0, 60.0], [25.0, 60.0], [25.0, 61.0], [24.0, 61.0], [24.0, 60.0]]],
+      },
     },
     {
       "type": "Feature",
       "properties": { "name": "Spain" },
       "geometry": {
         "type": "Polygon",
-        "coordinates": [[[0.0, 40.0], [1.0, 40.0], [1.0, 41.0], [0.0, 41.0], [0.0, 40.0]]]
-      }
-    }
-  ]
+        "coordinates": [[[0.0, 40.0], [1.0, 40.0], [1.0, 41.0], [0.0, 41.0], [0.0, 40.0]]],
+      },
+    },
+  ],
 };
